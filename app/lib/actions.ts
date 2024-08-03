232956dcea587ba83c0e6c6de5d6b58f5d42aa66
naopeke <naopeke@protonmail.com>
2024-08-03 13:58:32 +0200

    ch14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,9 +14,15 @@ import { redirect } from 'next/navigation';
 //import Zod and define a schema that matches the shape of your form object. This schema will validate the formData before saving it to a database.
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'],{
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
   });
    
@@ -26,15 +32,42 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 //Tip: If you're working with forms that have many fields, you may want to consider using the entries() method with JavaScript's Object.fromEntries()
 //const rawFormData = Object.fromEntries(formData.entries())
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
+  export async function createInvoice(prevState: State, formData: FormData) {
+      // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+   // If form validation fails, return errors early. Otherwise, continue.
+   if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+
+
+// export async function createInvoice(formData: FormData) {
     // const rawFormData = {
 
     //pass your rawFormData to CreateInvoice to validate the types:
-    const { customerId, amount, status } = CreateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
+    // const { customerId, amount, status } = CreateInvoice.parse({
+    //   customerId: formData.get('customerId'),
+    //   amount: formData.get('amount'),
+    //   status: formData.get('status'),
+    // });
     // Test it out:
     // console.log(rawFormData);
     // console.log(typeof rawFormData.amount);
@@ -44,37 +77,91 @@ export async function createInvoice(formData: FormData) {
     //create a new date with the format "YYYY-MM-DD" for the invoice's creation date:
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try {
+        await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+          message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+// export async function updateInvoice(id: string, formData: FormData) {
+//     const { customerId, amount, status } = UpdateInvoice.parse({
+//       customerId: formData.get('customerId'),
+//       amount: formData.get('amount'),
+//       status: formData.get('status'),
+//     });
+   
+//     const amountInCents = amount * 100;
+   
+//     try {
+//         await sql`
+//             UPDATE invoices
+//             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+//             WHERE id = ${id}
+//           `;
+//     } catch (error) {
+//         return { message: 'Database Error: Failed to Update Invoice.' };
+//     }
+   
+//     revalidatePath('/dashboard/invoices');
+//     redirect('/dashboard/invoices');
+//   }
+
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
    
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+   
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
 
-  export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+
+export async function deleteInvoice(id: string) {
+    // below is to make error.tsx
+    // throw new Error('Failed to Delete Invoice');
+
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
   }
 
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,15 +9,24 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
+//import the useActionState hook from react. Since useActionState is a hook, you will need to turn your form into a Client Component using "use client" directive:
+import { useActionState } from 'react';
+
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
     // import the createInvoice from your actions.ts file. Add a action attribute to the <form> element, and call the createInvoice action.
     //In HTML, you'd pass a URL to the action attribute. This URL would be the destination where your form data should be submitted (usually an API endpoint).
     // However, in React, the action attribute is considered a special prop - meaning React builds on top of it to allow actions to be invoked.
     // Behind the scenes, Server Actions create a POST API endpoint. This is why you don't need to create API endpoints manually when using Server Actions.
-    <form action={createInvoice}>
+   
+    // <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -28,6 +39,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error" //error using the form state.
             >
               <option value="" disabled>
                 Select a customer
@@ -40,6 +52,17 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+
+              {/* error using state */}
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+        {state.errors?.customerId &&
+          state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
+      </div>
+
         </div>
 
         {/* Invoice Amount */}
@@ -56,6 +79,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
